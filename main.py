72a42eb13ca815a686da6e10d2d11b535acec8a1
sugeedarou <linus.schilpp@googlemail.com>
2021-04-28 15:26:19 +0200

    add one-hot encoding for sentence

--- main.py
+++ main.py
@@ -9,61 +9,61 @@ timit_path = Path('../..//ML_DATA/timit')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 train_dataset = TimitDataset(root=timit_path, train=True, frame_length=50)
-frames, labels = train_dataset[0]
 
 
 num_classes = Phoneme.phoneme_count()
-num_epochs = 2
-batch_size = 100
+num_epochs = 15
+batch_size = 1
 learning_rate = 0.001
 
 input_size = train_dataset.samples_per_frame
 hidden_size = 128
 num_layers = 2
 
+
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                           batch_size=1, 
+                                           batch_size=batch_size, 
                                            shuffle=True)
 
 class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+    def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
-        self.num_layers = num_layers
         self.hidden_size = hidden_size
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
 
-    def forward(self, x):
-        frame_count = x.size(1)
-        # print(x.shape)
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
+    def forward(self, input_tensor, hidden_tensor):
+        combined = torch.cat((input_tensor, hidden_tensor), 1)
         
-        out, _ = self.rnn(x, h0)
-        
-        predictions = torch.zeros(frame_count, num_classes)
-
-        for i in range(frame_count):
-            predictions[i] = self.fc(out[:, i, :])
-
-        return predictions
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+    
+    def init_hidden(self):
+        return torch.zeros(1, self.hidden_size).to(device)
         
 
-model = RNN(input_size, hidden_size, num_layers, num_classes).to(device)
+model = RNN(input_size, hidden_size, num_classes).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
 
 # train
 n_total_steps = len(train_loader)
 for epoch in range(num_epochs):
-    for i, (frames, labels) in enumerate(train_loader): 
+    for i, (sentence, frames, labels) in enumerate(train_loader): 
+        loss = torch.tensor(0)
         frames = frames.to(device)
         labels = labels.flatten().to(device)
+
+        hidden = model.init_hidden()
+
+        outputs = torch.zeros(frames.size(1), num_classes).to(device)
+        for j in range(frames.size(1)):
+            output, hidden = model(frames[0][j].view(1, -1), hidden)
+            outputs[j] = output.view(num_classes)
         
-        outputs = model(frames).to(device)
-        # print('#####')
-        # print(frames.shape)
-        # print(outputs.shape)
-        # print(labels.shape)
         loss = criterion(outputs, labels)
         
         optimizer.zero_grad()
--- utils.py
+++ utils.py
@@ -4,10 +4,15 @@ import torch
 import librosa
 from math import floor, ceil
 import numpy as np
+import string
 import warnings
 # disable C++ extension warning
 warnings.filterwarnings('ignore', 'torchaudio C\+\+', )
 import torchaudio
+import torchaudio.transforms as T
+
+all_characters = list(string.ascii_lowercase) + \
+                 [',', ';', '.', '!', '?', ':', '-', '\'', '\"', ' ']
 
 class Phoneme():
     all_phonemes = [
@@ -74,8 +79,26 @@ def get_labels_from_file(path, samples_per_frame, n_samples):
         sample_idx += samples_per_frame
 
     return torch.tensor(labels)
-    
 
+def get_sentence_from_file(path):
+    sentence = ''
+    with open(path) as f:
+        line = f.readline().rstrip()
+        line2 = line[line.index(' ')+1:]
+        sentence = line2[line2.index(' ') + 1:]
+    return sentence
+
+def one_hot_encode(e, l):
+    return [1 if e == x else 0 for x in l]
+
+def encode_sentence(s):
+    s = [one_hot_encode(c, all_characters) for c in s.lower()]
+    s = torch.FloatTensor(s).flatten()
+    return s
+
+def get_encoded_sentence_from_file(path):
+    return encode_sentence(get_sentence_from_file(path))
+    
 
 def get_recording_paths(root, train):
     recording_paths = []
@@ -105,7 +128,7 @@ class TimitDataset(torch.utils.data.Dataset):
         first_recording = self.data / f'{self.recording_paths[0]}.WAV'
         _, self.sampling_rate = torchaudio.load(first_recording)
         self.samples_per_frame = floor(self.sampling_rate / 1000 * self.frame_length)
-
+        self.specgram_hop_length = 100
 
     def resample(self, waveform, sampling_rate):
         if sampling_rate != self.sampling_rate:
@@ -118,6 +141,7 @@ class TimitDataset(torch.utils.data.Dataset):
         recording_path = self.recording_paths[index]
         wav_path = self.data / f'{recording_path}.WAV'
         pn_path = self.data / f'{recording_path}.PHN'
+        sentence_path = self.data / f'{recording_path}.TXT'
 
         waveform, sampling_rate = torchaudio.load(wav_path)
         waveform = self.resample(waveform[0], self.sampling_rate)
@@ -125,12 +149,32 @@ class TimitDataset(torch.utils.data.Dataset):
         n_samples = len(waveform)
         n_frames = ceil(n_samples / self.samples_per_frame)
         frames = waveform.unfold(0, self.samples_per_frame, self.samples_per_frame)
-        # specgram = torchaudio.transforms.MelSpectrogram()(frames[0])
-        # print(specgram)
+
+        # n_fft = 1024
+        # win_length = None
+        # hop_length = 42
+        # n_mels = 128
+
+        # mel_spectrogram = T.MelSpectrogram(
+        #     sample_rate=self.sampling_rate,
+        #     n_fft=n_fft,
+        #     win_length=win_length,
+        #     hop_length=hop_length,
+        #     center=True,
+        #     pad_mode="reflect",
+        #     power=2.0,
+        #     norm='slaney',
+        #     onesided=True,
+        #     n_mels=n_mels,
+        # )
+        # specgram = mel_spectrogram(frames[0])
+        # print(specgram.shape)
         # exit()
 
+        sentence = get_encoded_sentence_from_file(sentence_path).view(1, -1)
         labels = get_labels_from_file(pn_path, self.samples_per_frame, n_samples)
-        return frames, labels
+        
+        return sentence, frames, labels
 
     def __len__(self):
         return self.n_recordings
