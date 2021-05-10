8e192a89248bfbf6f50e35af1a45ca70971cbc51
sugeedarou <linus.schilpp@googlemail.com>
2021-05-10 12:34:41 +0200

    fix validation

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from torch.utils.data import DataLoader, random_split
 from torch.nn.utils.rnn import pad_sequence
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 import pytorch_lightning as pl
+from pytorch_lightning.metrics import functional as FM
 
 from settings import *
 from dataset import TimitDataset
@@ -21,7 +22,7 @@ from utils import sentence_characters
 num_classes = Phoneme.phoneme_count()
 num_epochs = 100
 batch_size = 64
-learning_rate = 0.001
+learning_rate = 0.01
 input_size = SPECGRAM_N_MELS
 
 
@@ -66,10 +67,8 @@ class TimitDataModule(pl.LightningDataModule):
                           **self.ds_args)
     
 
-
-class PhonemeClassifier(pl.LightningModule):
-
-    def __init__(self, batch_size, lr):
+class ClassificationModel(nn.Module):
+    def __init__(self):
         super().__init__()
         self.num_layers1 = 2
         self.num_layers2 = 2
@@ -78,9 +77,6 @@ class PhonemeClassifier(pl.LightningModule):
         self.rnn1 = nn.RNN(SPECGRAM_N_MELS, self.hidden_size1, self.num_layers1, batch_first=True, bidirectional=True, dropout=0.5)
         self.rnn2 = nn.GRU(self.hidden_size1*2, self.hidden_size2, self.num_layers1, batch_first=True, bidirectional=True, dropout=0.5)
         self.fc = nn.Linear(self.hidden_size2*2, num_classes)
-        self.criterion = nn.CrossEntropyLoss()
-        self.batch_size = batch_size
-        self.lr = lr
 
     def forward(self, x, lengths):
         predictions = torch.zeros(lengths.sum().item(), num_classes).cuda(non_blocking=True)
@@ -99,38 +95,63 @@ class PhonemeClassifier(pl.LightningModule):
             for j in range(lengths[i]):
                 predictions[p] = self.fc(out2[0][j])
                 p += 1
-
         return predictions
 
-    def training_step(self, train_batch, batch_idx):
-        (specgrams, lengths), labels = train_batch
 
-        outputs = self.forward(specgrams, lengths)
+class PhonemeClassifier(pl.LightningModule):
+
+    def __init__(self, batch_size, lr):
+        super().__init__()
+        self.batch_size = batch_size
+        self.lr = lr
+        self.model = ClassificationModel()
+        self.criterion = nn.CrossEntropyLoss()
+    
+    def training_step(self, batch, batch_idx):
+        (specgrams, lengths), labels = batch
+
+        outputs = self.model(specgrams, lengths)
         loss = self.criterion(outputs, labels)
         self.log('train_loss', loss)
         return loss
 
-    def validation_step(self, val_batch, batch_idx):
-        (specgrams, lengths), labels = val_batch
+    def validation_step(self, batch, batch_idx):
+        (specgrams, lengths), labels = batch
         specgrams = specgrams
         labels = labels
 
-        outputs = self.forward(specgrams, lengths)
+        outputs = self.model(specgrams, lengths)
         loss = self.criterion(outputs, labels)
-        self.log('val_loss', loss, prog_bar=True)
+        acc = FM.accuracy(torch.argmax(outputs, dim=1), labels)
+
+        metrics = {'val_loss': loss, 'val_acc': acc}
+        self.log_dict(metrics, prog_bar=True)
+        return metrics
+
+    def test_step(self, batch, batch_idx):
+        metrics = self.validation_step(batch, batch_idx)
+        metrics = {'test_loss': metrics['val_loss'], 'test_acc': metrics['val_acc']}
+        self.log_dict(metrics, prog_bar=True)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        lr_schedulers = {'scheduler': ReduceLROnPlateau(optimizer, patience=4),
+        lr_schedulers = {'scheduler': ReduceLROnPlateau(optimizer, patience=0),
                          'monitor': 'val_loss'}
         return [optimizer], [lr_schedulers]
+
+    # hide v_num in progres bar
+    def get_progress_bar_dict(self):
+        tqdm_dict = super().get_progress_bar_dict()
+        if 'v_num' in tqdm_dict:
+            del tqdm_dict['v_num']
+        return tqdm_dict
 
 
 if __name__ == '__main__':
     dm = TimitDataModule()
-
+    
     model = PhonemeClassifier(batch_size, learning_rate)
-    trainer = pl.Trainer(gpus=1, max_epochs=100)
+    trainer = pl.Trainer(gpus=1, max_epochs=15)#, resume_from_checkpoint='lightning_logs/version_42/checkpoints/epoch=14-step=314.ckpt')
 
     trainer.fit(model, dm)
     trainer.test(datamodule=dm)
